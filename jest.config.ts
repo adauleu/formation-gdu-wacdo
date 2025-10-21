dca7ad16db0f27b9194d5297788e1651183f1cbf
Alban Dauleu <adauleu@gmail.com>
2025-10-21 16:22:44 +0200

    Changement des fichiers de config pour être compatible Vercel

--- jest.config.ts
+++ jest.config.ts
@@ -4,4 +4,5 @@ import { createDefaultEsmPreset } from 'ts-jest';
 /** @type {import("jest").Config} **/
 export default {
   ...createDefaultEsmPreset(),
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
 };

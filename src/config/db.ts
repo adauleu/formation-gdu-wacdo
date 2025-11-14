3870203b670faa511e5fea48f31b2d8cb335e13a
Alban Dauleu <adauleu@gmail.com>
2025-11-14 09:46:14 +0100

    Création d'une variable d'environnement pour le nom de la base MongoDb

--- src/config/db.ts
+++ src/config/db.ts
@@ -6,7 +6,9 @@ export async function connectDB() {
     }
 
     try {
-        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.34p6sfe.mongodb.net/wacdo?retryWrites=true&w=majority&appName=Cluster0`)
+        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.34p6sfe.mongodb.net?retryWrites=true&w=majority`, {
+            dbName: process.env.DB_NAME,
+        })
         console.log('Connected to the database');
     } catch (error) {
         console.error('Error connecting to the database', error);
